43b353de24ae579e4f0b85f5f4fe125d9ced4dce
islomboev_botir@mail.ru <islomboev_botir@mail.ru>
2022-09-20 22:10:26 +0300

    Bla bla car

--- Project13/Source.cpp
+++ Project13/Source.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 using namespace std;
+void Print(string str);
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-
+	Print("bla bla CAR");
 
 	return 0;
+}
+
+void Print(string str)
+{
+	cout << "Это строка - " << str;
+
 }
